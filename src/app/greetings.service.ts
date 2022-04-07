08da2c9a686d7683c4ba03237d483ae316e05d07
Onika Kim Asiao Dumbrique <kim.asiao@partners.w2m.com>
2022-04-07 10:46:34 +0200

    Inspector, bundles and tree-shaking of dependencies

--- src/app/greetings.service.ts
+++ src/app/greetings.service.ts
@@ -1,16 +1,14 @@
 import { Injectable } from "@angular/core";
-import { LoggerService } from "./logger.service";
+// import { LoggerService } from "./logger.service";
 
 //escribir Injectable en todos los servicios que se creen
 @Injectable()
 export class GreetingsService {
   public counter = 0;
-  constructor(private logger: LoggerService){
-
-  }
+  //constructor(private logger: LoggerService){}
 
   handleGreeting(message:string){
     this.counter++;
-    this.logger.log(message);
+    //this.logger.log(message);
   }
 }
