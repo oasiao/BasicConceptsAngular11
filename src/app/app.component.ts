cc709a0d104f1f364925973c175980ec9d4b23fd
Onika Kim Asiao Dumbrique <kim.asiao@partners.w2m.com>
2022-04-07 09:41:38 +0200

    ngclas

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -13,5 +13,14 @@ export class AppComponent {
   onSayHello(message:string){
     console.log(message);
   }
+
+  updateNameClasses(name:string){
+    return {
+      'error': name.length <=3,
+      'warning': name.length > 3 && name.length <=6,
+      'success': name.length > 6,
+      'bold': name.length > 8
+    }
+  }
 }
 
