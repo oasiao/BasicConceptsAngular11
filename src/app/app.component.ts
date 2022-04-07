2d4d47a7f2626f825ed473ef09a327bf4f0ab663
Onika Kim Asiao Dumbrique <kim.asiao@partners.w2m.com>
2022-04-07 09:47:18 +0200

    ngStyle

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,9 +9,13 @@ export class AppComponent {
   title = 'BasicConceptsAngular11';
   name:string = 'Kim Asiao';
   myName:string = 'Onika Kim Asiao Dumbrique';
+  public titleStyles = {};
+  public counter = 0;
 
   onSayHello(message:string){
     console.log(message);
+    this.counter++;
+    this.updateTitleStyles();
   }
 
   updateNameClasses(name:string){
@@ -20,6 +24,13 @@ export class AppComponent {
       'warning': name.length > 3 && name.length <=6,
       'success': name.length > 6,
       'bold': name.length > 8
+    }
+  }
+
+  updateTitleStyles(){
+    this.titleStyles = {
+      'margin-top': '40px',
+      'color': this.counter < 3 ? 'green' : 'goldenrod'
     }
   }
 }
